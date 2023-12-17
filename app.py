c752e5299c3e6983618236ad8d7ff4a6ce8d996c
Ceek350 <hafidzrivan4@gmail.com>
2023-12-17 20:03:33 +0700

    rndr tmplt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import MySQLdb.cursors
 import MySQLdb.cursors, re, hashlib
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.secret_key = 'zerostore'
 
